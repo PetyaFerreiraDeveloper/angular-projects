a91bf5793b744e80e723db3e4d9718b844bd3748
Petya Naydenova Ferreira <petianaidenova@gmail.com>
2022-11-09 09:08:55 +0100

    navigate to hero-details page

--- angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
+++ angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { IHero } from '../types/hero';
 
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { HeroService } from '../hero.service';
+
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
@@ -10,9 +14,24 @@ export class HeroDetailComponent implements OnInit {
 
   @Input() hero?: IHero;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) { }
 
   ngOnInit(): void {
+    this.getHero();
+  }
+
+  getHero(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 
 }
