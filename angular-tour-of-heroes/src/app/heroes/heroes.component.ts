f9115356185aebef36dbbab41903132cd4a2c0ee
Petya Naydenova Ferreira <petianaidenova@gmail.com>
2022-11-09 08:55:30 +0100

    create routing

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -11,24 +11,14 @@ import { MessageService } from '../message.service';
 export class HeroesComponent implements OnInit {
   heroes: IHero[] = [];
 
-  selectedHero?: IHero;
-
   constructor(private heroService: HeroService, private messageService: MessageService) { }
   
   ngOnInit(): void {
     this.getHeroes();
   }
 
-  onSelect(hero: IHero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
-  }
-
   getHeroes() : void {
     this.heroService.getHeroes() 
       .subscribe(heroes => this.heroes = heroes);
   }
-
-
-
 }
