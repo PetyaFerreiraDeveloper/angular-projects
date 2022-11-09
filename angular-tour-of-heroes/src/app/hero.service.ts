5c01495611141599255b0d04ae4e197e1a00d87f
Petya Naydenova Ferreira <petianaidenova@gmail.com>
2022-11-09 11:47:10 +0100

    update hero

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -58,6 +58,19 @@ export class HeroService {
       );
   }
 
+  /** PUT: update the hero on the server */
+  updateHero(hero: IHero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, this.httpOptions)
+      .pipe(
+        tap(_ => this.log(`updated hero id=${hero.id}`)),
+        catchError(this.handleError<any>('updateHero'))
+      );
+  }
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  }
+
   private log(message: string) {
     this.messageService.add(`HeroService: ${message}`);
   }
